fba8c81598d7ceffb09f8b28cf71f80cbdf0cf14
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-30 22:30:21 +0200

    Refactor `OrderItem.jsx` to include `formatCurrency` helper and minor code style updates

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -1,5 +1,7 @@
+import { formatCurrency } from '../../utils/helpers.js'
+
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
-  const { quantity, name, totalPrice } = item;
+  const { quantity, name, totalPrice } = item
 
   return (
     <li>
@@ -10,7 +12,7 @@ function OrderItem({ item, isLoadingIngredients, ingredients }) {
         <p>{formatCurrency(totalPrice)}</p>
       </div>
     </li>
-  );
+  )
 }
 
-export default OrderItem;
+export default OrderItem
