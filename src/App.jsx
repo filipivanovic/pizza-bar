7e2d05c14b2a20f8fd073ee7f286d6ed6e49fc57
filipivanovic <filip_ivanovic@hotmail.com>
2025-06-30 22:44:55 +0200

    Add React Router setup and define routes for core app pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './ui/Home.jsx'
+import Menu from './features/menu/Menu.jsx'
+import Order from './features/order/Order.jsx'
+import Cart from './features/cart/Cart.jsx'
+import CreateOrder from './features/order/CreateOrder.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/menu',
+    element: <Menu />
+  },
+  {
+    path: '/cart',
+    element: <Cart />
+  },
+  {
+    path: '/order/new',
+    element: <CreateOrder />
+  },
+  {
+    path: '/order/:id',
+    element: <Order />
+  }
+])
+
 const App = () => {
-  return (
-    <div>
-      <h1>hello vite</h1>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
