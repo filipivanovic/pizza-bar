4b05eefe696bad5ab5eb826c4638c7b2773076a2
filipivanovic <filip_ivanovic@hotmail.com>
2025-07-01 09:23:43 +0200

    Add `SearchOrder` component and integrate it into the `Header` for order navigation

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom'
+import SearchOrder from '../features/order/SearchOrder.jsx'
 
 const Header = () => {
   return (
     <header>
       <Link to={`/`}>Pizza Bar</Link>
+      <SearchOrder />
     </header>
   )
 }
