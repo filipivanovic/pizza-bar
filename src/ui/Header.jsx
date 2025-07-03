271398e12449def6cd0f090bb982688759c36e55
filipivanovic <filip_ivanovic@hotmail.com>
2025-07-03 23:19:33 +0200

    Refine `index.html`, `Header`, and `tailwind.config.js`: add Google Fonts integration, update `Header` component with new font, and extend Tailwind theme with custom font and color

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from '../features/user/Username.jsx'
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase">
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 font-sans uppercase">
       <Link to={`/`} className="tracking-widest">
         Pizza Bar
       </Link>
