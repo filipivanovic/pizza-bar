8a182d6b77e97623b22f7bfc1e32dcf98784edec
filipivanovic <filip_ivanovic@hotmail.com>
2025-07-03 22:41:08 +0200

    Refine `Cart` component: extract link into reusable `LinkButton` component and update link styling

---
+++ src/ui/LinkButton.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+const LinkButton = ({ children, to }) => {
+  return (
+    <Link to={to} className="text-sm text-blue-500 hover:text-blue-700 hover:underline">
+      {children}
+    </Link>
+  )
+}
+
+export default LinkButton
