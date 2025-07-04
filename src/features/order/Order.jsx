e3e06949365c590a1f3f853114b315a54f3fe71a
filipivanovic <filip_ivanovic@hotmail.com>
2025-07-04 19:52:57 +0200

    Refine `Order` component: enhance layout with Tailwind classes for improved spacing, readability, and visual consistency in order details and pricing sections

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -22,7 +22,7 @@ function Order() {
 
   return (
     <div className="space-y-8 px-4 py-6">
-      <div className="flex flex-wrap items-center justify-between">
+      <div className="flex flex-wrap items-center justify-between gap-2">
         <h2 className={`text-xl font-semibold`}>Order #{id} status</h2>
 
         <div className={`space-x-2`}>
@@ -41,8 +41,8 @@ function Order() {
         </div>
       </div>
 
-      <div>
-        <p>
+      <div className="flex flex-wrap items-center justify-between gap-2 bg-stone-200 px-6 py-5">
+        <p className={`text-xs font-medium text-stone-500`}>
           {deliveryIn >= 0
             ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
             : 'Order should have arrived'}
@@ -50,10 +50,18 @@ function Order() {
         <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
       </div>
 
-      <div>
-        <p>Price pizza: {formatCurrency(orderPrice)}</p>
-        {priority && <p>Price priority: {formatCurrency(priorityPrice)}</p>}
-        <p>To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
+      <div className={`space-y-2 bg-stone-200 px-6 py-5`}>
+        <p className={`text-sm font-medium text-stone-600`}>
+          Price pizza: {formatCurrency(orderPrice)}
+        </p>
+        {priority && (
+          <p className={`text-sm font-medium text-stone-600`}>
+            Price priority: {formatCurrency(priorityPrice)}
+          </p>
+        )}
+        <p className={`text-sm font-bold text-stone-600`}>
+          To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
+        </p>
       </div>
     </div>
   )
