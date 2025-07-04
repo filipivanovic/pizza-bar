224311b9e7c4acb791ca28cdd661a2731cc19cdf
filipivanovic <filip_ivanovic@hotmail.com>
2025-07-04 18:31:24 +0200

    Refine `MenuItem` component: integrate `Button` component for "Add to cart" functionality and adjust layout with Tailwind classes

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,4 +1,5 @@
 import { formatCurrency } from '../../utils/helpers.js'
+import Button from '../../ui/Button.jsx'
 
 // eslint-disable-next-line react/prop-types
 function MenuItem({ pizza }) {
@@ -8,16 +9,17 @@ function MenuItem({ pizza }) {
   return (
     <li className={`flex gap-4 py-2`}>
       <img src={imageUrl} alt={name} className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`} />
-      <div className="flex flex-col">
+      <div className="flex grow flex-col">
         <p className={`font-medium`}>{name}</p>
         {/* eslint-disable-next-line react/prop-types */}
         <p className={`text-sm capitalize italic text-stone-600`}>{ingredients.join(', ')}</p>
-        <div className="mt-auto">
+        <div className="mt-auto flex items-center justify-between">
           {!soldOut ? (
             <p className="text-sm">{formatCurrency(unitPrice)}</p>
           ) : (
             <p className="text-sm font-medium uppercase text-stone-500">Sold out</p>
           )}
+          <Button>Add to cart</Button>
         </div>
       </div>
     </li>
