f81a45a3a83af4bbf68fdc6af37730303b7bbbb1
filipivanovic <filip_ivanovic@hotmail.com>
2025-07-04 22:58:42 +0200

    Enhance user flow: use Redux to manage username state across `Cart`, `CreateOrder`, and `Home` components, pre-fill forms, and conditionally render UI based on user login state.

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,6 +1,9 @@
 import CreateUser from '../features/user/CreateUser.jsx'
+import { useSelector } from 'react-redux'
+import Button from './Button.jsx'
 
 function Home() {
+  const username = useSelector(state => state.user.username)
   return (
     <div className="my-10 px-4 text-center sm:my-16">
       <h1 className="mb-8 text-xl font-semibold md:text-3xl">
@@ -9,7 +12,13 @@ function Home() {
         <span className="text-yellow-500">Straight out of the oven, straight to you.</span>
       </h1>
 
-      <CreateUser className="" />
+      {username === '' ? (
+        <CreateUser className="" />
+      ) : (
+        <Button type={`primary`} to={`/menu`}>
+          Continue ordering, {username}
+        </Button>
+      )}
     </div>
   )
 }
