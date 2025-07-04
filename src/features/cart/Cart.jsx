8f24827e613c939fc3e0b3dc79b8a17990f13692
filipivanovic <filip_ivanovic@hotmail.com>
2025-07-04 19:10:18 +0200

    Remove redundant `Clear cart` button from `Cart` component

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -46,7 +46,6 @@ function Cart() {
           Order pizzas
         </Button>
         <Button type={`secondary`}>Clear cart</Button>
-        <button>Clear cart</button>
       </div>
     </div>
   )
