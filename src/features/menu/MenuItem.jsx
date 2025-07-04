d7960a1cd2c624e8408ffefe926ed58be3f90114
filipivanovic <filip_ivanovic@hotmail.com>
2025-07-04 18:46:31 +0200

    Refine `Cart` and `MenuItem` components: enhance `Button` usage with `type` prop for consistent styling and adjust layout with Tailwind classes

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -9,7 +9,7 @@ function MenuItem({ pizza }) {
   return (
     <li className={`flex gap-4 py-2`}>
       <img src={imageUrl} alt={name} className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`} />
-      <div className="flex grow flex-col">
+      <div className="flex grow flex-col pt-0.5">
         <p className={`font-medium`}>{name}</p>
         {/* eslint-disable-next-line react/prop-types */}
         <p className={`text-sm capitalize italic text-stone-600`}>{ingredients.join(', ')}</p>
